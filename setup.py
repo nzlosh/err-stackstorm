0576dedd17d9b008b9ea295166e9abc4b534b5a6
Carlos <nzlosh@yahoo.com>
2018-11-29 07:29:01 +0100

    Version bump err-stackstorm to 2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="err-stackstorm",
-    version="1.4.0",
+    version="2.0.0",
     author="Err-StackStorm Plugin contributors",
     author_email="nobody@localhost",
     description="An Errbot plugin for StackStorm ChatOps.",
