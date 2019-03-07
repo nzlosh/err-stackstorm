bfe589f595b375cb8b638e573cdd6fd3cb018f86
Carlos <nzlosh@yahoo.com>
2019-03-07 10:10:54 +0100

    Start to list features that require unit tests.

--- test_err-stackstorm.py
+++ test_err-stackstorm.py
@@ -12,6 +12,35 @@ pytest_plugins = ["errbot.backends.test"]
 extra_plugin_dir = '.'
 
 
+# secret_store
+## get a secret
+## set a secret
+## delete a secret
+## list secrets
+
+# access_control
+# create a session
+# get a session by session_id
+# get a session by chat_user
+# delete a session by session_id
+# delete a session by chat_user
+# list sessions
+# set an st2 token by chat_user
+# get an st2 token by session_id
+
+# chat_adapters
+# slack
+# xmpp
+# mattermost
+# rocketchat
+# gitter
+# discord
+
+# bot_commands
+# display help
+# match and execute action-alias
+
+
 # session tests
 def test_session():
     user_id = "test_id"
