88e872751660896c07e50f7cef07d12cebec8f1b
Carlos <nzlosh@yahoo.com>
2019-03-22 09:18:34 +0100

    Add tests for SessionManager class and fix PEP8 errors.

--- test_err-stackstorm.py
+++ test_err-stackstorm.py
@@ -1,22 +1,25 @@
 # coding:utf-8
 # import os
 import time
-# import sys
 # import st2
+
 import pytest
+from mock import Mock
+
 from lib.session import Session
+from lib.session_manager import SessionManager
+from lib.store_adapters import ClearTextStoreAdapter, KeyringStoreAdapter, VaultStoreAdapter
 from lib.errors import SessionExpiredError, SessionInvalidError, SessionConsumedError, \
     SessionExistsError
 
 pytest_plugins = ["errbot.backends.test"]
 extra_plugin_dir = '.'
 
-
 # secret_store
-## get a secret
-## set a secret
-## delete a secret
-## list secrets
+# get a secret
+# set a secret
+# delete a secret
+# list secrets
 
 # access_control
 # create a session
@@ -41,8 +44,66 @@ extra_plugin_dir = '.'
 # match and execute action-alias
 
 
-# session tests
+def test_session_manager():
+    """
+    SessionManager class tests
+    """
+    cfg = Mock()
+    cfg.secrets_store = "cleartext"
+
+    user_id = 'test%user'
+    user_secret = 'secret_for_test'
+    user_token = '1234567890-0987654321'
+
+    session_manager = SessionManager(cfg)
+
+    # Have requested backend
+    assert isinstance(session_manager.secure_store, ClearTextStoreAdapter)
+
+    # create a new user
+    s = session_manager.create(user_id, user_secret)
+    assert s.user_id == user_id
+
+    # 1 session in list
+    assert len(session_manager.list_sessions()) == 1
+
+    # create same user raise exception
+    with pytest.raises(SessionExistsError):
+        session_manager.create(user_id, user_secret)
+
+    # get an existing session
+    s1 = session_manager.get_by_uuid(s.id())
+    assert s1.id() == s.id()
+
+    # get a non-existent session
+    with pytest.raises(SessionInvalidError):
+        session_manager.get_by_uuid(s.id() + "no_uuid")
+
+    # get an existing session by user
+    s2 = session_manager.get_by_userid(user_id)
+    assert s2.id() == s.id()
+
+    # get a non-existent session by user
+    with pytest.raises(SessionInvalidError):
+        session_manager.get_by_userid(user_id + "no_user")
+
+    # confirm existence
+    assert session_manager.exists(user_id) is True
+
+    # confirm non-existence
+    assert session_manager.exists(user_id+"no_user") is False
+
+    # Set value in secret store
+    assert session_manager.put_secret(s.id(), user_token) is True
+
+    # Get value in secret store
+    assert session_manager.get_secret(s.id()) == user_token
+
+
 def test_session():
+    """
+    Session class tests
+    """
     user_id = "test_id"
     secret = "test_secret"
     session = Session(user_id, secret)
