41dcfef1c733f72c6116b8e72aded81adf8defe4
Carlos <nzlosh@yahoo.com>
2022-08-27 18:31:30 +0200

    WIP - fix test_enquiry

--- tests/test_enquiry.py
+++ tests/test_enquiry.py
@@ -14,23 +14,24 @@ def test_enquiry():
     Enquiry class tests
     """
     user_id = "test_id"
-enquiry_data = {
-    "id": "60a7c8876d573fae8028be34",
-    "route": "slack_query",
-    "ttl": 1440,
-    "users": [],
-    "roles": [],
-    "schema": {
-        "type": "object",
-        "properties": {
-            "password": {
-                "type": "string",
-                "description": "Run this workflow (yes/no)",
-                "required": True,
+
+    enquiry_data = {
+        "id": "60a7c8876d573fae8028be34",
+        "route": "slack_query",
+        "ttl": 1440,
+        "users": [],
+        "roles": [],
+        "schema": {
+            "type": "object",
+            "properties": {
+                "password": {
+                    "type": "string",
+                    "description": "Run this workflow (yes/no)",
+                    "required": True,
+                },
             },
         },
-    },
-}
+    }
     enquiry_id = enquiry_data["id"]
 
     enquiry = Enquiry(enquiry_data)
