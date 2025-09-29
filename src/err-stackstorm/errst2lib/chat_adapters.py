46ad82cd9a3e7be0c5ea3fa69eb986f624ea023b
Carlos <nzlosh@yahoo.com>
2025-09-29 15:23:44 +0200

    Bump version to 2.3.2 (#53)
    * Add debug
    
    * Format with Black
    
    * Set correct version in documentation and drop comments from sphinx config.
    
    * Bump version to 2.3.2

--- src/err-stackstorm/errst2lib/chat_adapters.py
+++ src/err-stackstorm/errst2lib/chat_adapters.py
@@ -405,7 +405,7 @@ class SlackChatAdapter(GenericChatAdapter):
         else:
             LOG.warning("target_id is type %s that isn't a Person or Room!", type(target_id))
 
-        LOG.debug(f"Sending Slack Block {extra}")
+        LOG.debug("Sending Slack Block %s", extra)
         self.bot_plugin._bot.slack_web.api_call("chat.postMessage", data=extra)
 
     def format_help(self, help_strings):
